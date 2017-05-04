9a4498bf7a43dc7c2d51579d3b6ac80e8575a7a4
Sanket Sudake <sanket@infracloud.io>
2017-05-04 05:16:54 +0000

    WIP: Add Cinder support for GCE
    1. Cinder Driver
    2. Adding SSH keys
    
    Change-Id: I5b2a9ee964fc239153d6de9886cc5e4a8e743c17
    Signed-off-by: Sanket <sanket@infracloud.io>

---
+++ cinder/volume/drivers/gce/driver.py
@@ -0,0 +1,135 @@
+# Copyright 2017 Platform9 Systems Inc.(http://www.platform9.com)
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_log import log as logging
+
+from cinder.i18n import _LI
+from cinder.volume.driver import BaseVD
+from cinder.volume.drivers.gce import gceconf
+from cinder.volume.drivers.gce import gceutils
+
+LOG = logging.getLogger(__name__)
+
+
+class GceDriver(BaseVD):
+    def __init__(self, *args, **kwargs):
+        super(GceDriver, self).__init__(*args, **kwargs)
+        self.VERSION = '0.0.1'
+        self.gce_zone = gceconf.zone
+        self.gce_region = gceconf.region
+        self.gce_project = gceconf.project_id
+        self.gce_svc_key = gceconf.service_key_path
+
+    def do_setup(self, context):
+        self.gce_svc = gceutils.get_gce_service(self.gce_svc_key)
+        self.set_initialized()
+        LOG.info(
+            _LI("Gce volume driver init with %s project, %s zone") %
+            (self.gce_project, self.gce_zone))
+
+    def _gce_volume_name(self, volume):
+        return 'vol-' + volume.id
+
+    def _gce_snapshot_name(self, snapshot):
+        return 'snap-' + snapshot.id
+
+    def create_volume(self, volume):
+        compute, project, zone = self.gce_svc, self.gce_project, self.gce_zone
+        name = self._gce_volume_name(volume)
+        size = volume['size']
+        operation = gceutils.create_disk(compute, project, zone, name, size)
+        gceutils.wait_for_operation(compute, project, operation)
+
+    def delete_volume(self, volume):
+        compute, project, zone = self.gce_svc, self.gce_project, self.gce_zone
+        name = self._gce_volume_name(volume)
+        operation = gceutils.delete_disk(compute, project, zone, name)
+        gceutils.wait_for_operation(compute, project, operation)
+
+    def check_for_setup_error(self):
+        pass
+
+    def create_export(self, context, volume, connector):
+        pass
+
+    def ensure_export(self, context, volume):
+        pass
+
+    def remove_export(self, context, volume):
+        pass
+
+    def initialize_connection(self, volume, connector, initiator_data=None):
+        compute, project, zone = self.gce_svc, self.gce_project, self.gce_zone
+        name = self._gce_volume_name(volume)
+        gce_volume = gceutils.get_disk(compute, project, zone, name)
+        return dict(data=gce_volume)
+
+    def terminate_connection(self, volume, connector, **kwargs):
+        pass
+
+    def _update_volume_stats(self):
+        data = dict()
+        data['volume_backend_name'] = 'gce'
+        data['vendor_name'] = 'Google, Inc.'
+        data['driver_version'] = '0.0.1'
+        data['storage_protocol'] = 'iscsi'
+        pool = dict(pool_name=gceconf.gce_pool_name,
+                    free_capacity_gb=gceconf.gce_free_capacity_gb,
+                    total_capacity_gb=gceconf.gce_free_capacity_gb,
+                    provisioned_capacity_gb=0, reserved_percentage=0,
+                    location_info=dict(), QoS_support=False,
+                    max_over_subscription_ratio=1.0,
+                    thin_provisioning_support=False,
+                    thick_provisioning_support=True, total_volumes=0)
+        data['pools'] = [pool]
+        self._stats = data
+
+    def get_volume_stats(self, refresh=False):
+        if refresh:
+            self._update_volume_stats()
+        return self._stats
+
+    def create_snapshot(self, snapshot):
+        compute, project, zone = self.gce_svc, self.gce_project, self.gce_zone
+        volume_name = self._gce_volume_name(snapshot.volume)
+        name = self._gce_snapshot_name(snapshot)
+        operation = gceutils.snapshot_disk(compute, project, zone, volume_name,
+                                           name)
+        gceutils.wait_for_operation(compute, project, operation)
+
+    def delete_snapshot(self, snapshot):
+        compute, project = self.gce_svc, self.gce_project
+        name = self._gce_snapshot_name(snapshot)
+        operation = gceutils.delete_snapshot(compute, project, name)
+        gceutils.wait_for_operation(compute, project, operation)
+
+    def create_volume_from_snapshot(self, volume, snapshot):
+        compute, project, zone = self.gce_svc, self.gce_project, self.gce_zone
+        name = self._gce_volume_name(volume)
+        snapshot_name = self._gce_snapshot_name(snapshot)
+        operation = gceutils.create_disk_from_snapshot(compute, project, zone,
+                                                       name, snapshot_name)
+        gceutils.wait_for_operation(compute, project, operation)
+
+    def copy_image_to_volume(self, context, volume, image_service, image_id):
+        raise NotImplementedError()
+
+    def copy_volume_to_image(self, context, volume, image_service, image_meta):
+        raise NotImplementedError()
+
+    def migrate_volume(self, context, volume, host):
+        raise NotImplementedError()
+
+    def copy_volume_data(self, context, src_vol, dest_vol, remote=None):
+        raise NotImplementedError()
---
+++ cinder/volume/drivers/gce/gceutils.py
@@ -0,0 +1,180 @@
+# Copyright (c) 2017 Platform9 Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from googleapiclient.discovery import build
+from oauth2client.client import GoogleCredentials
+from oslo_log import log as logging
+
+from cinder.i18n import _LI
+from oslo_service import loopingcall
+
+LOG = logging.getLogger(__name__)
+
+
+class GceOperationError(Exception):
+    pass
+
+
+def wait_for_operation(compute, project, operation, interval=1, timeout=60):
+    """Wait for GCE operation to complete, raise error if operation failure
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param zone: string, GCE Name of zone
+    :param operation: object, Operation resource obtained by calling GCE API
+    :param interval: int, Time period(seconds) between two GCE operation checks
+    :param timeout: int, Absoulte time period(seconds) to monitor GCE operation
+    """
+
+    def watch_operation(name, request):
+        result = request.execute()
+        if result['status'] == 'DONE':
+            LOG.info(
+                _LI("Operation %s status is %s") % (name, result['status']))
+            if 'error' in result:
+                raise GceOperationError(result['error'])
+            raise loopingcall.LoopingCallDone()
+
+    operation_name = operation['name']
+
+    if 'zone' in operation:
+        zone = operation['zone'].split('/')[-1]
+        monitor_request = compute.zoneOperations().get(
+            project=project, zone=zone, operation=operation_name)
+    elif 'region' in operation:
+        region = operation['region'].split('/')[-1]
+        monitor_request = compute.regionOperations().get(
+            project=project, region=region, operation=operation_name)
+    else:
+        monitor_request = compute.globalOperations().get(
+            project=project, operation=operation_name)
+
+    timer = loopingcall.FixedIntervalWithTimeoutLoopingCall(
+        watch_operation, operation_name, monitor_request)
+    timer.start(interval=interval, timeout=timeout).wait()
+
+
+def get_gce_service(service_key):
+    """Returns GCE compute resource object for interacting with GCE API
+    :param service_key: string, Path of service key obtained from
+        https://console.cloud.google.com/apis/credentials
+    :return: :class:`Resource <Resource>` object
+    :rtype: googleapiclient.discovery.Resource
+    """
+    credentials = GoogleCredentials.from_stream(service_key)
+    service = build('compute', 'v1', credentials=credentials)
+    return service
+
+
+def create_disk(compute, project, zone, name, size):
+    """Create disk in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param zone: string, GCE Name of zone
+    :param name: string, GCE disk name
+    :param size: int, size of disk inn Gb
+    :return: Operation information
+    :rtype: dict
+    """
+    body = {
+        "name": name,
+        "zone": "projects/%s/zones/%s" % (project, zone),
+        "type": "projects/%s/zones/%s/diskTypes/pd-standard" % (project, zone),
+        "sizeGb": size
+    }
+    return compute.disks().insert(project=project, zone=zone, body=body,
+                                  sourceImage=None).execute()
+
+
+def delete_disk(compute, project, zone, name):
+    """Delete disk in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param zone: string, GCE Name of zone
+    :param name: string, GCE disk name
+    :return: Operation information
+    :rtype: dict
+    """
+    return compute.disks().delete(project=project, zone=zone,
+                                  disk=name).execute()
+
+
+def get_disk(compute, project, zone, name):
+    """Get info of disk in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param zone: string, GCE Name of zone
+    :param name: string, GCE disk name
+    :return: GCE disk information
+    :rtype: dict
+    """
+    return compute.disks().get(project=project, zone=zone, disk=name).execute()
+
+
+def snapshot_disk(compute, project, zone, name, snapshot_name):
+    """Create snapshot of disk in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param zone: string, GCE Name of zone
+    :param name: string, GCE disk name
+    :param snapshot_name: string, GCE snapshot name
+    :return: Operation information
+    :rtype: dict
+    """
+    body = {"name": snapshot_name}
+    return compute.disks().createSnapshot(project=project, zone=zone,
+                                          disk=name, body=body).execute()
+
+
+def get_snapshot(compute, project, name):
+    """Get info of snapshot in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param name: string, GCE snapshot name
+    :return: GCE snapshot information
+    :rtype: dict
+    """
+    return compute.snapshots().get(project=project, snapshot=name).execute()
+
+
+def delete_snapshot(compute, project, name):
+    """Delete snapshot in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param name: string, GCE snapshot name
+    :return: Operation information
+    :rtype: dict
+    """
+    return compute.snapshots().delete(project=project, snapshot=name).execute()
+
+
+def create_disk_from_snapshot(compute, project, zone, name, snapshot_name):
+    """Create disk from snapshot in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param zone: string, GCE Name of zone
+    :param name: string, GCE disk name
+    :param snapshot_name: string, GCE snapshot name
+    :return: Operation information
+    :rtype: dict
+    """
+    gce_snapshot = get_snapshot(compute, project, snapshot_name)
+    body = {
+        "name": name,
+        "zone": "projects/%s/zones/%s" % (project, zone),
+        "type": "projects/%s/zones/%s/diskTypes/pd-standard" % (project, zone),
+        "sourceSnapshot": gce_snapshot["selfLink"],
+        "sizeGb": gce_snapshot["diskSizeGb"]
+    }
+    return compute.disks().insert(project=project, zone=zone, body=body,
+                                  sourceImage=None).execute()
