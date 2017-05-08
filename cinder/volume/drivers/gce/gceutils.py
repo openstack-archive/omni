cdd83b35a955bdef47ab2e2277cf56c325bf9c06
Sanket <sanket@infracloud.io>
2017-05-08 11:12:28 +0530

    Added Nova Instace Snapshot Operation and security group rules support
    1. Added instance snapshot feature
    2. Support for security group rules using GCE firewall rules
    2. Added disk, snapshot related operations nova gceutils library
    3. Cleanup unwanted operations from glance gceutils library
    
    Change-Id: I1777e99cf4e1f7c230c4a9b286e4c01e8deb3d2f
    Signed-off-by: Sanket <sanket@infracloud.io>

--- cinder/volume/drivers/gce/gceutils.py
+++ cinder/volume/drivers/gce/gceutils.py
@@ -12,18 +12,58 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import time
+
 from googleapiclient.discovery import build
 from oauth2client.client import GoogleCredentials
 from oslo_log import log as logging
 
-from cinder.i18n import _LI
+from cinder.i18n import _LI, _
 from oslo_service import loopingcall
+from oslo_utils import reflection
 
 LOG = logging.getLogger(__name__)
 
 
 class GceOperationError(Exception):
     pass
+
+
+class _FixedIntervalWithTimeoutLoopingCall(loopingcall.LoopingCallBase):
+    """A fixed interval looping call with timeout checking mechanism."""
+
+    _RUN_ONLY_ONE_MESSAGE = _("A fixed interval looping call with timeout"
+                              " checking and can only run one function at"
+                              " at a time")
+
+    _KIND = _('Fixed interval looping call with timeout checking.')
+
+    def start(self, interval, initial_delay=None, stop_on_exception=True,
+              timeout=0):
+        start_time = time.time()
+
+        def _idle_for(result, elapsed):
+            delay = round(elapsed - interval, 2)
+            if delay > 0:
+                func_name = reflection.get_callable_name(self.f)
+                LOG.warning('Function %(func_name)r run outlasted '
+                            'interval by %(delay).2f sec',
+                            {'func_name': func_name,
+                             'delay': delay})
+            elapsed_time = time.time() - start_time
+            if timeout > 0 and elapsed_time > timeout:
+                raise loopingcall.LoopingCallTimeOut(
+                    _('Looping call timed out after %.02f seconds') %
+                    elapsed_time)
+            return -delay if delay < 0 else 0
+
+        return self._start(_idle_for, initial_delay=initial_delay,
+                           stop_on_exception=stop_on_exception)
+
+
+if not hasattr(loopingcall, 'FixedIntervalWithTimeoutLoopingCall'):
+    loopingcall.FixedIntervalWithTimeoutLoopingCall = \
+        _FixedIntervalWithTimeoutLoopingCall
 
 
 def wait_for_operation(compute, project, operation, interval=1, timeout=60):
