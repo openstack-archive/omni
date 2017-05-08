392f6b26a33c5490da2c431bb8be4e14d6416753
Sanket <sanket@infracloud.io>
2017-05-08 11:12:28 +0530

    Added Nova Instace Snapshot Operation
    1. Added instance snapshot feature
    2. Added disk, snapshot related operations nova gceutils library
    3. Cleanup unwanted operations from glance gceutils library
    
    Change-Id: I1777e99cf4e1f7c230c4a9b286e4c01e8deb3d2f

--- nova/gce/driver.py
+++ nova/gce/driver.py
@@ -18,17 +18,18 @@ import uuid
 
 import nova.conf
 from nova import exception
+from nova.image import glance
 from nova.i18n import _LI
 from nova.virt import driver, hardware
 from oslo_config import cfg
 from oslo_log import log as logging
+from nova.compute import task_states
 
 import gceutils
 
 from nova.virt.gce.constants import GCE_STATE_MAP
 
 LOG = logging.getLogger(__name__)
-
 gce_group = cfg.OptGroup(name='GCE',
                          title='Options to connect to Google cloud')
 
@@ -243,7 +244,7 @@ class GCEDriver(driver.ComputeDriver):
         operation = gceutils.create_instance(compute, project, zone,
                                              instance_name, image_link,
                                              flavor_link, network_interfaces)
-        gceutils.wait_for_operation(compute, project, zone, operation)
+        gceutils.wait_for_operation(compute, project, operation)
         gce_instance = gceutils.get_instance(compute, project, zone,
                                              instance_name)
         # Update GCE info in openstack instance metadata
@@ -260,7 +261,7 @@ class GCEDriver(driver.ComputeDriver):
         operation = gceutils.set_instance_metadata(
             compute, project, zone, gce_instance['name'], gce_metadata,
             operation='add')
-        gceutils.wait_for_operation(compute, project, zone, operation)
+        gceutils.wait_for_operation(compute, project, operation)
         self._uuid_to_gce_instance[instance.uuid] = gceutils.get_instance(
             compute, project, zone, instance_name)
 
@@ -269,8 +270,75 @@ class GCEDriver(driver.ComputeDriver):
         :param context: security context
         :param instance: nova.objects.instance.Instance
         :param image_id: Reference to a pre-created image holding the snapshot.
+
+        Steps:
+        1. Find boot disk
+        2. Stop instance
+        3. Create temporary boot disk snapshot
+        4. Start instance
+        5. Create temporary disk from snapshot
+        6. Create image from disk
+        7. Add Image info to glance
+        8. Delete temporary disk
+        9. Delete temporary snapshot
         """
-        raise NotImplementedError()
+        compute, project, zone = self.gce_svc, self.gce_project, self.gce_zone
+        gce_id = self._get_gce_id_from_instance(instance)
+        LOG.info(_LI("Taking snapshot of instance %s") % instance.uuid)
+        boot_disk = gceutils.get_instance_boot_disk(compute, project, zone,
+                                                    gce_id)
+        operation = gceutils.stop_instance(compute, project, zone, gce_id)
+        gceutils.wait_for_operation(compute, project, operation)
+        disk_name = boot_disk['name']
+        snapshot_name = 'novasnap-' + disk_name
+        operation = gceutils.snapshot_disk(compute, project, zone,
+                                           boot_disk['name'], snapshot_name)
+        gceutils.wait_for_operation(compute, project, operation)
+        operation = gceutils.start_instance(compute, project, zone, gce_id)
+        gceutils.wait_for_operation(compute, project, operation)
+        snapshot_disk_name = 'vol-' + snapshot_name
+        operation = gceutils.create_disk_from_snapshot(
+            compute, project, zone, snapshot_disk_name, snapshot_name)
+        gceutils.wait_for_operation(compute, project, operation)
+        snapshot_disk_info = gceutils.get_disk(compute, project, zone,
+                                               snapshot_disk_name)
+        update_task_state(task_state=task_states.IMAGE_PENDING_UPLOAD)
+        image_api = glance.get_default_image_service()
+        image_data = image_api.show(context, image_id)
+        name = image_data['name']
+        operation = gceutils.create_image_from_disk(
+            compute, project, name, snapshot_disk_info['selfLink'])
+        gceutils.wait_for_operation(compute, project, operation, timeout=120)
+        LOG.info(
+            _LI("Created GCE image %s from instance %s") % (name,
+                                                            instance.uuid))
+        update_task_state(task_state=task_states.IMAGE_UPLOADING,
+                          expected_state=task_states.IMAGE_PENDING_UPLOAD)
+        gce_img_data = gceutils.get_image(compute, project, name)
+        image_metadata = {
+            'name': name,
+            'container_format': 'bare',
+            'disk_format': 'raw',
+            'is_public': False,
+            'status': 'active',
+            'properties': {
+                'image_state': 'available',
+                'owner_id': instance.project_id,
+                'ramdisk_id': instance.ramdisk_id,
+                'location': 'gce://%s/%s/%s' % (project, name, image_id),
+                'gce_image_id': gce_img_data['id'],
+                'gce_link': gce_img_data['selfLink'],
+                'gce_size': gce_img_data['diskSizeGb']
+            },
+        }
+        image_api.update(context, image_id, image_metadata)
+        disk_operation = gceutils.delete_disk(compute, project, zone,
+                                              snapshot_disk_name)
+        snap_operation = gceutils.delete_snapshot(compute, project,
+                                                  snapshot_name)
+        gceutils.wait_for_operation(compute, project, disk_operation)
+        gceutils.wait_for_operation(compute, project, snap_operation)
+        LOG.info(_LI("Completed snapshot for instance %s") % instance.uuid)
 
     def reboot(self, context, instance, network_info, reboot_type,
                block_device_info=None, bad_volumes_callback=None):
@@ -303,10 +371,10 @@ class GCEDriver(driver.ComputeDriver):
         gce_id = self._get_gce_id_from_instance(instance)
         LOG.info(_LI('Stopping instance %s') % instance.uuid)
         operation = gceutils.stop_instance(compute, project, zone, gce_id)
-        gceutils.wait_for_operation(compute, project, zone, operation)
+        gceutils.wait_for_operation(compute, project, operation)
         LOG.info(_LI('Starting instance %s') % instance.uuid)
         operation = gceutils.start_instance(compute, project, zone, gce_id)
-        gceutils.wait_for_operation(compute, project, zone, operation)
+        gceutils.wait_for_operation(compute, project, operation)
         LOG.info(_LI('Soft Reboot Complete for instance %s') % instance.uuid)
 
     def _hard_reboot(self, context, instance, network_info,
@@ -315,7 +383,7 @@ class GCEDriver(driver.ComputeDriver):
         gce_id = self._get_gce_id_from_instance(instance)
         LOG.info(_LI('Resetting instance %s') % instance.uuid)
         operation = gceutils.reset_instance(compute, project, zone, gce_id)
-        gceutils.wait_for_operation(compute, project, zone, operation)
+        gceutils.wait_for_operation(compute, project, operation)
         LOG.info(_LI('Hard Reboot Complete %s') % instance.uuid)
 
     @staticmethod
@@ -370,7 +438,7 @@ class GCEDriver(driver.ComputeDriver):
         gce_id = self._get_gce_id_from_instance(instance)
         LOG.info(_LI('Stopping instance %s') % instance.uuid)
         operation = gceutils.stop_instance(compute, project, zone, gce_id)
-        gceutils.wait_for_operation(compute, project, zone, operation)
+        gceutils.wait_for_operation(compute, project, operation)
         LOG.info(_LI('Power off complete %s') % instance.uuid)
 
     def power_on(self, context, instance, network_info, block_device_info):
@@ -379,7 +447,7 @@ class GCEDriver(driver.ComputeDriver):
         gce_id = self._get_gce_id_from_instance(instance)
         LOG.info(_LI('Starting instance %s') % instance.uuid)
         operation = gceutils.start_instance(compute, project, zone, gce_id)
-        gceutils.wait_for_operation(compute, project, zone, operation)
+        gceutils.wait_for_operation(compute, project, operation)
         LOG.info(_LI("Power on Complete %s") % instance.uuid)
 
     def soft_delete(self, instance):
@@ -452,7 +520,7 @@ class GCEDriver(driver.ComputeDriver):
         gce_id = self._get_gce_id_from_instance(instance)
         LOG.info(_LI('Deleting instance %s') % instance.uuid)
         operation = gceutils.delete_instance(compute, project, zone, gce_id)
-        gceutils.wait_for_operation(compute, project, zone, operation)
+        gceutils.wait_for_operation(compute, project, operation)
         LOG.info(_LI("Destroy Complete %s") % instance.uuid)
 
     def attach_volume(self, context, connection_info, instance, mountpoint,
@@ -466,7 +534,7 @@ class GCEDriver(driver.ComputeDriver):
         disk_link = gce_volume['selfLink']
         operation = gceutils.attach_disk(compute, project, zone, gce_id,
                                          disk_name, disk_link)
-        gceutils.wait_for_operation(compute, project, zone, operation)
+        gceutils.wait_for_operation(compute, project, operation)
         LOG.info(
             _LI("Volume %s attached to instace %s") % (disk_name,
                                                        instance.uuid))
@@ -481,7 +549,7 @@ class GCEDriver(driver.ComputeDriver):
         disk_name = gce_volume['name']
         operation = gceutils.detach_disk(compute, project, zone, gce_id,
                                          disk_name)
-        gceutils.wait_for_operation(compute, project, zone, operation)
+        gceutils.wait_for_operation(compute, project, operation)
         LOG.info(
             _LI("Volume %s detached from instace %s") % (disk_name,
                                                          instance.uuid))
--- nova/gce/gceutils.py
+++ nova/gce/gceutils.py
@@ -13,14 +13,19 @@
 #    under the License.
 
 import six
-import time
 from oslo_log import log as logging
 
 from nova.i18n import _LI
 from googleapiclient.discovery import build
 from oauth2client.client import GoogleCredentials
+from oslo_service import loopingcall
+from six.moves import urllib
 
 LOG = logging.getLogger(__name__)
+
+
+class GceOperationError(Exception):
+    pass
 
 
 def list_instances(compute, project, zone):
@@ -210,8 +215,7 @@ def reset_instance(compute, project, zone, name):
                                      instance=name).execute()
 
 
-def wait_for_operation(compute, project, zone, operation, interval=1,
-                       timeout=60):
+def wait_for_operation(compute, project, operation, interval=1, timeout=60):
     """Wait for GCE operation to complete, raise error if operation failure
     :param compute: GCE compute resource object using googleapiclient.discovery
     :param project: string, GCE Project Id
@@ -220,23 +224,33 @@ def wait_for_operation(compute, project, zone, operation, interval=1,
     :param interval: int, Time period(seconds) between two GCE operation checks
     :param timeout: int, Absoulte time period(seconds) to monitor GCE operation
     """
-    operation_name = operation['name']
-    if interval < 1:
-        raise ValueError("wait_for_operation: Interval should be positive")
-    iterations = timeout / interval
-    for i in range(iterations):
-        result = compute.zoneOperations().get(
-            project=project, zone=zone, operation=operation_name).execute()
+
+    def watch_operation(name, request):
+        result = request.execute()
         if result['status'] == 'DONE':
-            LOG.info("Operation %s status is %s" % (operation_name,
-                                                    result['status']))
+            LOG.info(
+                _LI("Operation %s status is %s") % (name, result['status']))
             if 'error' in result:
-                raise Exception(result['error'])
-            return result
-        time.sleep(interval)
-    raise Exception(
-        "wait_for_operation: Operation %s failed to perform in timeout %s" %
-        (operation_name, timeout))
+                raise GceOperationError(result['error'])
+            raise loopingcall.LoopingCallDone()
+
+    operation_name = operation['name']
+
+    if 'zone' in operation:
+        zone = operation['zone'].split('/')[-1]
+        monitor_request = compute.zoneOperations().get(
+            project=project, zone=zone, operation=operation_name)
+    elif 'region' in operation:
+        region = operation['region'].split('/')[-1]
+        monitor_request = compute.regionOperations().get(
+            project=project, region=region, operation=operation_name)
+    else:
+        monitor_request = compute.globalOperations().get(
+            project=project, operation=operation_name)
+
+    timer = loopingcall.FixedIntervalWithTimeoutLoopingCall(
+        watch_operation, operation_name, monitor_request)
+    timer.start(interval=interval, timeout=timeout).wait()
 
 
 def get_gce_service(service_key):
@@ -337,3 +351,129 @@ def detach_disk(compute, project, zone, instance_name, disk_name):
     return compute.instances().detachDisk(project=project, zone=zone,
                                           instance=instance_name,
                                           deviceName=disk_name).execute()
+
+
+def get_instance_boot_disk(compute, project, zone, instance):
+    """Return boot disk info for instance
+    """
+    gce_instance = get_instance(compute, project, zone, instance)
+    for disk in gce_instance['disks']:
+        if disk['boot']:
+            disk_url = disk['source']
+            items = urllib.parse.urlparse(disk_url).path.strip('/').split('/')
+            if len(items) < 4 or items[-2] != 'disks':
+                LOG.error(_LI('Invalid disk URL %s') % (disk_url))
+            disk_name, zone = items[-1], items[-3]
+            disk_info = get_disk(compute, project, zone, disk_name)
+            return disk_info
+    # We should never reach here
+    raise AssertionError("Boot disk not found for instance %s" % instance)
+
+
+def create_disk(compute, project, zone, name, size):
+    """Create disk in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param zone: string, GCE Name of zone
+    :param name: string, GCE disk name
+    :param size: int, size of disk inn Gb
+    :return: Operation information
+    :rtype: dict
+    """
+    body = {
+        "name": name,
+        "zone": "projects/%s/zones/%s" % (project, zone),
+        "type": "projects/%s/zones/%s/diskTypes/pd-standard" % (project, zone),
+        "sizeGb": size
+    }
+    return compute.disks().insert(project=project, zone=zone, body=body,
+                                  sourceImage=None).execute()
+
+
+def delete_disk(compute, project, zone, name):
+    """Delete disk in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param zone: string, GCE Name of zone
+    :param name: string, GCE disk name
+    :return: Operation information
+    :rtype: dict
+    """
+    return compute.disks().delete(project=project, zone=zone,
+                                  disk=name).execute()
+
+
+def get_disk(compute, project, zone, name):
+    """Get info of disk in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param zone: string, GCE Name of zone
+    :param name: string, GCE disk name
+    :return: GCE disk information
+    :rtype: dict
+    """
+    return compute.disks().get(project=project, zone=zone, disk=name).execute()
+
+
+def snapshot_disk(compute, project, zone, name, snapshot_name):
+    """Create snapshot of disk in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param zone: string, GCE Name of zone
+    :param name: string, GCE disk name
+    :param snapshot_name: string, GCE snapshot name
+    :return: Operation information
+    :rtype: dict
+    """
+    body = {"name": snapshot_name}
+    return compute.disks().createSnapshot(project=project, zone=zone,
+                                          disk=name, body=body).execute()
+
+
+def get_snapshot(compute, project, name):
+    """Get info of snapshot in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param name: string, GCE snapshot name
+    :return: GCE snapshot information
+    :rtype: dict
+    """
+    return compute.snapshots().get(project=project, snapshot=name).execute()
+
+
+def delete_snapshot(compute, project, name):
+    """Delete snapshot in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param name: string, GCE snapshot name
+    :return: Operation information
+    :rtype: dict
+    """
+    return compute.snapshots().delete(project=project, snapshot=name).execute()
+
+
+def create_disk_from_snapshot(compute, project, zone, name, snapshot_name):
+    """Create disk from snapshot in GCE
+    :param compute: GCE compute resource object using googleapiclient.discovery
+    :param project: string, GCE Project Id
+    :param zone: string, GCE Name of zone
+    :param name: string, GCE disk name
+    :param snapshot_name: string, GCE snapshot name
+    :return: Operation information
+    :rtype: dict
+    """
+    gce_snapshot = get_snapshot(compute, project, snapshot_name)
+    body = {
+        "name": name,
+        "zone": "projects/%s/zones/%s" % (project, zone),
+        "type": "projects/%s/zones/%s/diskTypes/pd-standard" % (project, zone),
+        "sourceSnapshot": gce_snapshot["selfLink"],
+        "sizeGb": gce_snapshot["diskSizeGb"]
+    }
+    return compute.disks().insert(project=project, zone=zone, body=body,
+                                  sourceImage=None).execute()
+
+
+def create_image_from_disk(compute, project, name, disk_link):
+    body = {"sourceDisk": disk_link, "name": name, "rawDisk": {}}
+    return compute.images().insert(project=project, body=body).execute()
