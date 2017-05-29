db6491c884746eefc096068433d42cabd1bb96f8
Pratik Shah <pratik@infracloud.io>
2017-05-29 17:01:05 +0530

    Unit Testcases for GCE Nova Driver
    Additional data files in gce/data:
    
    1. Fake service key file(omni.json)
    2. Data required to generate Gce Service(service_data.json)
    3. Fake disk data for attach_volume and detach_volume API(get_disk.json)
    4. Fake instance data(get_instance.json)
    5. Fake list of instances(list_instances.json)
    6. Machines information for flavor information(list_machines_info.json)
    
    Change-Id: Ib8bf42c93e4a2f8f1c821a26b2868dfda993eadd

---
+++ nova/tests/gce/gce_mock.py
@@ -0,0 +1,88 @@
+# Copyright (c) 2017 Platform9 Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import six
+from apiclient.http import HttpMock
+from googleapiclient.discovery import build
+
+DATA_DIR = os.path.dirname(os.path.abspath(__file__)) + '/data'
+
+
+def fake_operation():
+    return {'name': 'fake_operation'}
+
+
+def get_gce_service(service_key):
+    http = HttpMock(DATA_DIR + '/service/service_data.json', {'status': '200'})
+    service = build('compute', 'v1', http=http, developerKey=service_key)
+    return service
+
+
+def list_instances(compute, project, zone):
+    http = HttpMock(DATA_DIR + '/instance/list_instances.json',
+                    {'status': '200'})
+    request = compute.instances().list(project=project, zone=zone)
+    response = request.execute(http=http)
+    return response
+
+
+def get_instance(compute, project, zone, instance):
+    http = HttpMock(DATA_DIR + '/instance/get_instance.json',
+                    {'status': '200'})
+    request = compute.instances().get(project=project, zone=zone,
+                                      instance=instance)
+    response = request.execute(http=http)
+    return response
+
+
+def get_instances_metadata_key(compute, project, zone, instance, key):
+    if isinstance(instance, six.string_types):
+        instance = get_instance(compute, project, zone, instance)
+    metadata = instance['metadata']
+    if 'items' in metadata:
+        for item in metadata['items']:
+            if item['key'] == key:
+                return item['value']
+    return None
+
+
+def get_machines_info(compute, project, zone):
+    http = HttpMock(DATA_DIR + '/machines_info/list_machines_info.json',
+                    {'status': '200'})
+    request = compute.machineTypes().list(project=project, zone=zone)
+    response = request.execute(http=http)
+    GCE_MAP = {
+        machine_type['name']: {
+            'memory_mb': machine_type['memoryMb'],
+            'vcpus': machine_type['guestCpus']
+        }
+        for machine_type in response
+    }
+    return GCE_MAP
+
+
+def get_disk(compute, project, zone, name):
+    http = HttpMock(DATA_DIR + '/disk/get_disk.json', {'status': '200'})
+    request = compute.disks().get(project=project, zone=zone, disk=name)
+    response = request.execute(http=http)
+    return response
+
+
+def create_anything(*args, **kwargs):
+    return fake_operation()
+
+
+def wait_for_operation(*args, **kwargs):
+    pass
