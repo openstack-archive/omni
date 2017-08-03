155bd22a22a04756789b56823d0d634f8fa5522f
Yusuf Kanchwala <yusuf@infracloud.io>
2017-08-03 16:47:10 +0530

    [AWS] Fix AWS Router Plugin Crashes
    1. Handle deprecated imports for Router Plugin
    2. Define AwsException at one place
    
    Change-Id: I2986934ef4a89fcc914ca3aebd33428565632810
    Closes-Bug: #1708573

--- neutron/neutron/plugins/ml2/drivers/aws/mechanism_aws.py
+++ neutron/neutron/plugins/ml2/drivers/aws/mechanism_aws.py
@@ -16,18 +16,14 @@ import random
 
 from neutron.callbacks import events
 from neutron.callbacks import resources
+from neutron.common.aws_utils import AwsException
 from neutron.common.aws_utils import AwsUtils
 from neutron import manager
 from neutron.plugins.ml2 import driver_api as api
 from neutron.plugins.ml2.drivers.aws import callbacks
-from neutron_lib.exceptions import NeutronException
 from oslo_log import log
 
 LOG = log.getLogger(__name__)
-
-
-class AwsException(NeutronException):
-    message = "AWS Error: '%(error_code)s' - '%(message)s'"
 
 
 class AwsMechanismDriver(api.MechanismDriver):
