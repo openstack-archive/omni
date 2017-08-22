4a4adb9679ef61724dd25aa44f792100ff23b817
Sanket <sanket@infracloud.io>
2017-08-22 14:58:34 +0530

    [WIP] Add Azure support for Neutron
    Change-Id: Ia38f37356852c50da5b65cb5c55d1e73ef4a8041
    Implements: blueprint azure-support

---
+++ neutron/neutron/common/azutils.py
@@ -0,0 +1,164 @@
+"""
+Copyright (c) 2017 Platform9 Systems Inc.
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied. See the
+License for the specific language governing permissions and limitations
+under the License.
+"""
+
+from msrestazure.azure_exceptions import CloudError
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.network import NetworkManagementClient
+from functools import partial
+from oslo_log import log as logging
+from neutron_lib import exceptions as n_exceptions
+from neutron.extensions import securitygroup as sg
+
+LOG = logging.getLogger(__name__)
+
+
+def get_credentials(tenant_id, client_id, client_secret):
+    credentials = ServicePrincipalCredentials(
+        client_id=client_id, secret=client_secret, tenant=tenant_id)
+    return credentials
+
+
+def _get_client(tenant_id, client_id, client_secret, subscription_id,
+                cls=None):
+    """Returns Azure compute resource object for interacting with Azure API
+
+    :param tenant_id: string, tenant_id from azure account
+    :param client_id: string, client_id (application id)
+    :param client_secret: string, secret key of application
+    :param subscription_id: string, unique identification id of account
+    :return: :class:`Resource <Resource>` object
+    """
+    credentials = get_credentials(tenant_id, client_id, client_secret)
+    client = cls(credentials, subscription_id)
+    return client
+
+
+get_compute_client = partial(_get_client, cls=ComputeManagementClient)
+get_network_client = partial(_get_client, cls=NetworkManagementClient)
+
+
+def _perform_and_wait(operation, args=(), kwargs={}, timeout=300):
+    operation(*args, **kwargs).wait(timeout=timeout)
+
+
+def create_network(network, resource_group, name, info):
+    _perform_and_wait(network.virtual_networks.create_or_update,
+                      (resource_group, name, info))
+
+
+def delete_network(network, resource_group, name):
+    _perform_and_wait(network.virtual_networks.delete, (resource_group, name))
+
+
+def get_network(network, resource_group, name):
+    try:
+        return network.virtual_networks.get(resource_group, name)
+    except CloudError:
+        raise n_exceptions.NetworkNotFound(net_id=name)
+
+
+def create_subnet(network, resource_group, network_name, name, info):
+    _perform_and_wait(network.subnets.create_or_update,
+                      (resource_group, network_name, name, info))
+
+
+def delete_subnet(network, resource_group, network_name, name):
+    _perform_and_wait(network.subnets.delete, (resource_group, network_name,
+                                               name))
+
+
+def get_subnet(network, resource_group, network_name, name):
+    try:
+        return network.subnets.get(resource_group, network_name, name)
+    except CloudError:
+        raise n_exceptions.SubnetNotFound(subnet_id=name)
+
+
+def get_nic(network, resource_group, name):
+    try:
+        return network.network_interfaces.get(resource_group, name)
+    except CloudError:
+        raise n_exceptions.PortNotFound(port_id=name)
+
+
+def create_nic(network, resource_group, name, body):
+    _perform_and_wait(network.network_interfaces.create_or_update,
+                      (resource_group, name, body))
+
+
+def delete_nic(network, resource_group, name):
+    _perform_and_wait(network.network_interfaces.delete, (resource_group,
+                                                          name))
+
+
+def create_sg(network, resource_group, name, body):
+    _perform_and_wait(network.network_security_groups.create_or_update,
+                      (resource_group, name, body))
+
+
+def delete_sg(network, resource_group, name):
+    _perform_and_wait(network.network_security_groups.delete, (resource_group,
+                                                               name))
+
+
+def create_sg_rule(network, resource_group, sg_name, name, body):
+    _perform_and_wait(network.security_rules.create_or_update,
+                      (resource_group, sg_name, name, body))
+
+
+def delete_sg_rule(network, resource_group, sg_name, name):
+    _perform_and_wait(network.security_rules.delete, (resource_group, sg_name,
+                                                      name))
+
+
+update_sg = create_sg
+update_sg_rule = create_sg_rule
+
+
+def convert_sg_rule(openstack_rule, priority):
+    directions = {'ingress': 'Inbound', 'egress': 'Outbound'}
+    protocols = {'tcp': 'Tcp', 'udp': 'Udp'}
+
+    sg_rule = {
+        'source_port_range': '*',
+        'destination_port_range': '*',
+        'source_address_prefix': '*',
+        'destination_address_prefix': '*',
+        'access': 'Allow',
+        'priority': priority
+    }
+    sg_rule['direction'] = directions[openstack_rule['direction']]
+
+    protocol = openstack_rule['protocol']
+    if protocol is None:
+        sg_rule['protocol'] = '*'
+    if protocol and protocol in protocols:
+        sg_rule['protocol'] = protocols[protocol]
+    else:
+        raise sg.SecurityGroupRuleInvalidProtocol(
+            protocol=protocol, values=protocols.keys())
+
+    port_range_min = openstack_rule['port_range_min']
+    port_range_max = openstack_rule['port_range_max']
+    if port_range_min and port_range_min == port_range_max:
+        sg_rule['destination_port_range'] = str(port_range_min)
+    elif port_range_min and port_range_max:
+        sg_rule['destination_port_range'] = "%s-%s" % (port_range_min,
+                                                       port_range_max)
+
+    if openstack_rule['remote_ip_prefix']:
+        # TODO(ssudake21): Allow support for tags in source_address_prefix
+        sg_rule['source_address_prefix'] = openstack_rule['remote_ip_prefix']
+
+    return sg_rule
