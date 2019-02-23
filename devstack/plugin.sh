283b57c49ef93207a384248f720106a4c44a1aab
Pratik Shah <pratik@infracloud.io>
2019-02-23 11:22:08 +0530

    Fixed devstack for AWS
    Change-Id: I9c93692eed47078c56eb91a247af5729cf56c84c

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -3,19 +3,22 @@ set -x
 
 cp /opt/stack/omni/devstack/lib/* $TOP_DIR/lib/
 sudo apt-get install crudini -y
-sudo pip install -r /opt/stack/omni/omni-requirements.txt
+sudo pip install -r /opt/stack/omni/requirements.txt
 
-if [[ "$1" == "stack" && "$2" == "pre-install" ]] && [ "$OMNI_PROVIDER" != "" ]; then
+if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
     if [ "$OMNI_PROVIDER" != "gce" ] && [ "$OMNI_PROVIDER" != "aws" ]; then
         echo "$OMNI_PROVIDER is not supported"
         exit 1
     fi
 fi
 
-if [[ "$1" == "stack" && "$2" == "extra" ]] && [ "$OMNI_PROVIDER" != "" ]; then
+if [[ "$1" == "stack" && "$2" == "extra" ]]; then
     source $TOP_DIR/lib/omni_$OMNI_PROVIDER
+    source $TOP_DIR/lib/common_functions
     configure_glance
     configure_cinder
     configure_nova
     configure_neutron
+    copy_omni_files
+    restart_services
 fi
